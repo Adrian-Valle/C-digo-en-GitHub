5c24427aceb706f0bf0ab26a40ba5d095cb99007
Adrian-Valle <vsadrian8@gmail.com>
2025-12-06 19:11:48 +0100

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Clonado con éxito!");
+        int numero = 42;
+        System.out.println("El significado de la vida es: " + numero);
+    }
+}
